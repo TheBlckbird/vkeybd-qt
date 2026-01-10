1870e5971ade89004ec3d66a2ae4038b03f37d93
TheBlckbird <theblckbrd@proton.me>
2026-01-11 00:59:36 +0100

    Update compile script to work on MacOS and Linux

--- compile.sh
+++ compile.sh
@@ -1,10 +1,25 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 #qmake-qt5 -o Makefile vkeybd-qt.pro
-qmake6 -o Makefile vkeybd-qt.pro 
-make -j $(nproc)
+qmake6 -o Makefile vkeybd-qt.pro
 
-if [ $? -eq 0 ]
-then
-    ./debug/vkeybd-qt
+os=$(uname -s)
+
+if [[ "$os" == "Linux" ]]; then
+    make -j $(nproc)
+
+    if [ $? -eq 0 ]
+    then
+        ./debug/vkeybd-qt
+    fi
+elif [[ "$os" == "Darwin" ]]; then
+    make -j $(sysctl -n hw.logicalcpu)
+
+    if [ $? -eq 0 ]
+    then
+        ./debug/vkeybd-qt.app/Contents/MacOS/vkeybd-qt
+    fi
+else
+    echo "not supported, Compilation currently only works on MacOS and Linux"
+    exit 1
 fi
